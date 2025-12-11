17b150f1b63d116b1a64ae7e2d651e66e3e36231
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-11 17:21:09 +0530

    Adding Template into app.js

--- react-todo-demo/my-first-react-app/src/App.js
+++ react-todo-demo/my-first-react-app/src/App.js
@@ -2,8 +2,35 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>We had created our first react application !!!</p>
+    <div>
+      <div>
+        <div>Todo's List</div>
+
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Feeding Animal</td>
+                <td>Eric Robby</td>
+              </tr>
+
+              <tr>
+                <th scope="row">2</th>
+                <td>Haircut</td>
+                <td>Eric Robby</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
