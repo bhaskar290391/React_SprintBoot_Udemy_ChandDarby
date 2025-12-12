f176b4652948b25c6e3445aa718b0d5f7656b4dd
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-12 16:14:15 +0530

    Typescripts: Variables

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,14 @@
+var found = false;
+var grade = 88.6;
+var firstName = "Bhaskar";
+var lastName = "Mudaliyar";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("The FirstName :" + firstName);
+console.log("The LastName :" + lastName);
+//Using Template String
+console.log("Hi ".concat(firstName, " ").concat(lastName));
+//Let Breaks it
+found = 88.6;
+grade = "bhaskar";
+firstName = true;
---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,17 @@
+let found: boolean = false;
+let grade: number = 88.6;
+let firstName: string = "Bhaskar";
+let lastName: string = "Mudaliyar";
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("The FirstName :" + firstName);
+console.log("The LastName :" + lastName);
+
+//Using Template String
+console.log(`Hi ${firstName} ${lastName}`);
+
+//Let Breaks it
+// found = 88.6;
+// grade = "bhaskar";
+// firstName = true;
