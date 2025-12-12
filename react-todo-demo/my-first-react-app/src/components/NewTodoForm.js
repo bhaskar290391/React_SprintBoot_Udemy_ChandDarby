e713790ee33c5368adfa36122946b0cdc5b4c22b
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-12 13:29:40 +0530

    Adding user input to React Form

--- react-todo-demo/my-first-react-app/src/components/NewTodoForm.js
+++ react-todo-demo/my-first-react-app/src/components/NewTodoForm.js
@@ -1,15 +1,32 @@
+import React, { useState } from "react";
+
 function NewTodoForm() {
+  const [description, setDescription] = useState("");
+  const [assigned, setAssigned] = useState("");
+
   return (
     <div className="mt-5">
       <form>
         <div className="mb-3">
           <label className="form-label">Assigned</label>
-          <input type="text" className="form-control" required></input>
+          <input
+            type="text"
+            className="form-control"
+            required
+            value={assigned}
+            onChange={(e) => setAssigned(e.target.value)}
+          ></input>
         </div>
 
         <div className="mb-3">
           <label className="form-label">Description</label>
-          <textarea className="form-control" required rows={3}></textarea>
+          <textarea
+            className="form-control"
+            required
+            rows={3}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
         </div>
 
         <button type="button" className="btn btn-primary">
